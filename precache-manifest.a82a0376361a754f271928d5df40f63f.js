3adcc79f28a24e029343f7beb293979f8b0644b0
Jahwan <jahwanoh@gmail.com>
2020-11-23 23:42:51 +0100

    Updates

---
+++ precache-manifest.a82a0376361a754f271928d5df40f63f.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da94fd1724ad01ebeb8c5843860aadc",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "f8aed5ce4422a1e48ac5",
+    "url": "/developerFolio/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "060ee8b496b69fa6fe48",
+    "url": "/developerFolio/static/css/4.ee2a0142.chunk.css"
+  },
+  {
+    "revision": "e7d9a221a3d14375e399",
+    "url": "/developerFolio/static/css/main.80c382ab.chunk.css"
+  },
+  {
+    "revision": "e3d34ab3d0799acb74ce",
+    "url": "/developerFolio/static/js/2.53fe0c48.chunk.js"
+  },
+  {
+    "revision": "f8aed5ce4422a1e48ac5",
+    "url": "/developerFolio/static/js/3.970b5ade.chunk.js"
+  },
+  {
+    "revision": "060ee8b496b69fa6fe48",
+    "url": "/developerFolio/static/js/4.3c938dc3.chunk.js"
+  },
+  {
+    "revision": "e7d9a221a3d14375e399",
+    "url": "/developerFolio/static/js/main.32d81427.chunk.js"
+  },
+  {
+    "revision": "0e7a921cef7dc202b43f",
+    "url": "/developerFolio/static/js/runtime-main.ee8d6ae1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "f676d330eb38a7cbf6b29b06349236f0",
+    "url": "/developerFolio/static/media/IMCOM-usag-y.f676d330.png"
+  },
+  {
+    "revision": "8ea1946e93a8e719a9748db0be713001",
+    "url": "/developerFolio/static/media/Metropolia logo.8ea1946e.png"
+  },
+  {
+    "revision": "d0643da730b021e2101aeec80e3992e4",
+    "url": "/developerFolio/static/media/Metropolia_logo.d0643da7.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/developerFolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "7d0b47cbda16532a8a7b233bad5f3005",
+    "url": "/developerFolio/static/media/bepro_logo.7d0b47cb.png"
+  },
+  {
+    "revision": "5a4b0993a686136dbdf091a58fb8b9d9",
+    "url": "/developerFolio/static/media/cau_new.5a4b0993.jpg"
+  },
+  {
+    "revision": "a698c16e83da0cad7713638aed505be4",
+    "url": "/developerFolio/static/media/cau_old.a698c16e.png"
+  },
+  {
+    "revision": "fc1a566612d5da3e07bb5f95b3e89ea2",
+    "url": "/developerFolio/static/media/cau_old_cut.fc1a5666.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/developerFolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/developerFolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "8f42e018a9340b6e4e20161b06525308",
+    "url": "/developerFolio/static/media/cpp.8f42e018.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/developerFolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/developerFolio/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/developerFolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "83a05b10639d238bd8c5bbab279ab911",
+    "url": "/developerFolio/static/media/harvardLogo.83a05b10.png"
+  },
+  {
+    "revision": "58a351f9b3fb86b36b4d5077a9ed8d42",
+    "url": "/developerFolio/static/media/hmobis_logo.58a351f9.png"
+  },
+  {
+    "revision": "07a02b0862517ca4bb5659bd6ff0f037",
+    "url": "/developerFolio/static/media/ipis_logo.07a02b08.png"
+  },
+  {
+    "revision": "e6833b643809d2d60b2d5bd9b5ad33d8",
+    "url": "/developerFolio/static/media/ipis_logo_.e6833b64.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/developerFolio/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/developerFolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "d878239eb762a9019d4c9e5aab91664d",
+    "url": "/developerFolio/static/media/loleu.d878239e.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "b1a39044e071e0e5c7ed1d0abb35f97f",
+    "url": "/developerFolio/static/media/mobis_logo.b1a39044.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/developerFolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/developerFolio/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/developerFolio/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/developerFolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/developerFolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/developerFolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/developerFolio/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/developerFolio/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "7f0feeedda08cf7d68593454bc2e82b5",
+    "url": "/developerFolio/static/media/talksCardBackAlt.7f0feeed.svg"
+  }
+]);
